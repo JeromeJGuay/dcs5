d505a1cfb841b7453f782731916905c90f0f96d1
jeromejguay <jerome.guay@protonmail.com>
2022-06-21 18:34:56 -0400

    simpler server

--- dcs5/main.py
+++ dcs5/main.py
@@ -5,6 +5,7 @@ from dcs5.cli_app import cli_app
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--server', action='store_true', default=False, help='start server.')
+    parser.add_argument('--test-server', action='store_true', default=False, help='start server.')
     parser.add_argument('--cli', action='store_true', default=False, help='start cli interface.')
 
     args, unknownargs = parser.parse_known_args()
@@ -14,6 +15,9 @@ def main():
     elif args.server is True:
         from dcs5.server import start_server
         start_server()
+    elif args.test_server is True:
+        from dcs5.server import start_server
+        start_server(start_controller=False)
     else:
         parser.print_help()
 
