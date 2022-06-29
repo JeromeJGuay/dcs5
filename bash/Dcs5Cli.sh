1f72e7df503e91651a718f889529769de8aa27dc
jeromejguay <jerome.guay@protonmail.com>
2022-06-29 14:40:38 -0400

    working on readme

--- bash/Dcs5Cli.sh
+++ bash/Dcs5Cli.sh
@@ -1,5 +1,5 @@
-conda activate dcs
+#conda activate dcs
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 PATH="${SCRIPT_DIR}../dcs5/cli_app.py"
-python3 ${PATH}
+python ${PATH}
 
