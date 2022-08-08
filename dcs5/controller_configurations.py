4157380650c56665d776a96c410d1f2ebee28493
jeromejguay <jerome.guay@protonmail.com>
2022-08-08 15:36:38 -0400

    Should be working.

--- dcs5/controller_configurations.py
+++ dcs5/controller_configurations.py
@@ -2,7 +2,7 @@
 Module that contains the scripts to load the board configurations.
 
 Valid commands :
-    ["BACKLIGHT_UP", "BACKLIGHT_DOWN", "CHANGE_STYLUS", "UNITS_mm", "UNITS_cm"]
+    ["BACKLIGHT_UP", "BACKLIGHT_DOWN", "CHANGE_STYLUS", "UNITS_mm", "UNITS_cm", "MODE", "MODE_TOP", "MODE_BOTTOM"]
 Valid keyboard inputs :
     ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'",
     '(', ')', '*', '+', ',', '-', '.', '/', '{', '|', '}', '~',
@@ -36,7 +36,7 @@ from typing import *
 
 from dcs5.utils import json2dict
 
-VALID_COMMANDS = ["BACKLIGHT_UP", "BACKLIGHT_DOWN", "CHANGE_STYLUS", "UNITS_mm", "UNITS_cm", "MODE"]
+VALID_COMMANDS = ["BACKLIGHT_UP", "BACKLIGHT_DOWN", "CHANGE_STYLUS", "UNITS_mm", "UNITS_cm", "MODE", "MODE_TOP", "MODE_BOTTOM"]
 VALID_SEGMENTS_MODE = ['length', 'top', 'bottom']
 VALID_KEYBOARD_KEYS = [
     '\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'",
@@ -150,11 +150,15 @@ class OutputModes:
 @dataclass
 class KeyMaps:
     control_box: Dict[str, str]
+    control_box_mode: dict[str, str]
     board: Dict[str, str]
+    board_mode: dict[str, str]
 
     def __post_init__(self):
         check_key_map(self.control_box)
         check_key_map(self.board)
+        check_key_map(self.control_box_mode)
+        check_key_map(self.board_mode)
 
 
 @dataclass
