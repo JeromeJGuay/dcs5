69990214cbf10c329e995c14f933da75b31118e8
jeromejguay <jerome.guay@protonmail.com>
2022-08-11 11:16:13 -0400

    adding PySimpleGui to Req

--- dcs5/gui.py
+++ dcs5/gui.py
@@ -1,8 +1,34 @@
-from dcs5.logger import init_logging
+"""
+
+
+
+
+
+VerticalSeparator(pad=None)
+
+"""
 import logging
+import PySimpleGUI as sg
+from dcs5.logger import init_logging
+
 
 
 def main():
-    init_logging(stdout_level=args.verbose, write=False)
+    init_logging(stdout_level='error', write=False)
+    logging.info('Starting Gui...')
 
-    pass
+
+
+
+
+def frame_example(window: sg.Window):
+    frame_layout = [
+        [sg.T('Text inside of a frame')],
+        [sg.CB('Check 1'), sg.CB('Check 2')],
+    ]
+    layout = [
+        [sg.Frame('My Frame Title', frame_layout, font='Any 12', title_color='blue')],
+        [sg.Submit(), sg.Cancel()]
+    ]
+
+    window = sg.Window('Frame with buttons', layout, font=("Helvetica", 12))
