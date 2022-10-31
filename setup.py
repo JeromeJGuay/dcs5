6e6ddca72a3dd6633299de1c8a6e58681e342104
jeromejguay <jerome.guay@protonmail.com>
2022-10-31 09:59:00 -0400

    change VERSION to 1.0.0

--- setup.py
+++ setup.py
@@ -11,10 +11,9 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/JeromeJGuay/dcs5",
     packages=find_packages(),
-    package_data={"": ["default_configs/*.json"]},
+    package_data={"": ["default_configs/*.json", "static/*"]},
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 3"],
     python_requires="~=3.10",
-    entry_points={"console_scripts": ["dcs5=dcs5.main:main", ]},
 )
 
